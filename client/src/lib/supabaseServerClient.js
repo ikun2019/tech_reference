2ec2b7dd25e97faf6370873c03e89010b2f3111b
ikun <0215.miyatto@gmail.com>
2025-10-11 17:12:21 +0900

    docker周りの修正

--- client/src/lib/supabaseServerClient.js
+++ client/src/lib/supabaseServerClient.js
@@ -1,11 +1,15 @@
+import fs from 'fs';
 import { cookies } from 'next/headers';
 import { createServerClient } from '@supabase/ssr';
+
+const SUPABASE_URL = fs.existsSync(process.env.SUPABASE_URL_FILE) ? fs.readFileSync(process.env.SUPABASE_URL_FILE, 'utf-8').trim() : process.env.NEXT_PUBLIC_SUPABASE_URL;
+const SUPABASE_ANON_KEY = fs.existsSync(process.env.SUPABASE_ANON_KEY_FILE) ? fs.readFileSync(process.env.SUPABASE_ANON_KEY_FILE, 'utf-8').trim() : process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const createSupabaseServerClient = async () => {
   const cookieStore = await cookies();
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY,
     {
       cookies: {
         getAll() { return cookieStore.getAll() },
